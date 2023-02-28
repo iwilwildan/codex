9a83df80ca895761614e80ba44be5822fc570e38
wildananacle <wildan.adli@anacle.com>
2023-02-28 16:04:48 +0900

    first commit

---
+++ client/script.js
@@ -0,0 +1,107 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+function typer(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueID() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAI, value, uniqueID) {
+  return `
+      <div class="wrapper ${isAI && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img src="${isAI ? bot : user}" alt="${isAI ? 'bot' : 'user'}" />
+          </div>
+          <div class="message" id="${uniqueID}">${value}</div>
+        </div>
+      </div>
+    `;
+}
+
+// e param is event
+const handleSubmit = async (e) => {
+  //default browser behavior when submit will reload page, we need to prevent default
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //render user's chatStripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  //bot's chatstripe
+  const uniqueID = generateUniqueID();
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueID);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueID);
+
+  loader(messageDiv);
+
+  //fetch data
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+  console.log(response.ok);
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typer(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Something went wrong..';
+    alert(err);
+  }
+};
+
+//call handleSubmit when submit triggered
+form.addEventListener('submit', handleSubmit);
+//call handleSubmit when hit enter (key 13)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) handleSubmit(e);
+});
